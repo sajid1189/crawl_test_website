e52364c0263cfe7406e910272ab9c23a9f0157b2
sajidur89 <sajid1189@gmail.com>
2019-03-01 16:28:04 +0100

    random connections generator initiated.

--- data/random_data_generator.py
+++ data/random_data_generator.py
@@ -2,12 +2,17 @@ import random
 import json
 import os
 
-from settings.constants import DATA_MAX_PAGES
+from settings.constants import DATA_MAX_PAGES, DEFAULT_MENU_PAGES, DATA_MAX_LINKS_PER_PAGE
 
 
-def gen_adjacency_sparse(fixed_menu=True):
-    url_prefix = "/pages/{}/"
-    for i in range(1,DATA_MAX_PAGES+1):
-        pass
+def gen_adjacency_sparse():
+    connections = dict()
+    for i in range(DATA_MAX_PAGES):
+        random_links = list([random.randint(1, DATA_MAX_PAGES) for _ in range(random.randint(5, DATA_MAX_LINKS_PER_PAGE))])
+        random_links = list(filter(lambda x: x not in DEFAULT_MENU_PAGES, random_links))
+        connections[i] = DEFAULT_MENU_PAGES + random_links
+    return connections
 
 
+if __name__ == '__main__':
+    gen_adjacency_sparse()
