5b1b4414a0720aa3e5ea2c08fc642a0331abe003
sajidur89 <sajid1189@gmail.com>
2019-03-01 15:26:33 +0100

    first commit

---
+++ data/random_data_generator.py
@@ -0,0 +1,13 @@
+import random
+import json
+import os
+
+from settings.constants import DATA_MAX_PAGES
+
+
+def gen_adjacency_sparse(fixed_menu=True):
+    url_prefix = "/pages/{}/"
+    for i in range(1,DATA_MAX_PAGES+1):
+        pass
+
+
