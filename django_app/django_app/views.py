15969055b42f2e5c4f262069268530fd18880444
Sajidur Rahman <sajid1189@gmail.com>
2019-03-05 22:04:03 +0100

    error handling with django messages, html enlarged.

--- django_app/django_app/views.py
+++ django_app/django_app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.contrib import messages
+
 from settings.constants import *
 
 from.models import *
@@ -15,11 +17,12 @@ class Page(TemplateView):
     def get_context_data(self, **kwargs):
         page_index = kwargs.get('page_index')
         current_site = Site.objects.filter(current=True).first()
+        context = super(Page, self).get_context_data(**kwargs)
         if not current_site:
-            return HttpResponse("Set the current site first")
+            messages.add_message(self.request, messages.ERROR, "Set the current site first")
+            return context
         ps = PageStructure.objects.get(site=current_site, page=page_index)
         links = json.loads(ps.links)
-        context = super(Page, self).get_context_data(**kwargs)
 
         links = [("page {}".format(link), "/page/{}".format(link)) for link in links]
         menu_items = [("menu {}".format(menu_item), "/page/{}".format(menu_item)) for menu_item in DEFAULT_MENU_PAGES]
