fd1b9fc27efb2b34a466deda943046128533184a
Sajidur Rahman <sajid1189@gmail.com>
2019-07-20 18:14:05 +0200

    improved the home page.

--- django_app/django_app/views.py
+++ django_app/django_app/views.py
@@ -5,9 +5,11 @@ from django.views.generic import TemplateView
 from django.contrib import messages
 
 sys.path.append(os.path.abspath('../'))
+sys.path.append(os.path.abspath('../../settings/'))
 
 from utils.links import link_maker, get_menu_links
 from.models import *
+from constants import DATA_MAX_PAGES, DEFAULT_MENU_PAGES, DATA_MAX_LINKS_PER_PAGE, DATA_MIN_LINKS_PER_PAGE
 
 
 class Home(TemplateView):
@@ -16,8 +18,13 @@ class Home(TemplateView):
     def get_context_data(self, **kwargs):
         first_page = PageStructure.objects.filter(site=Site.objects.filter(current=True).first()).first()
         context = super(Home, self).get_context_data(**kwargs)
+        current_site = Site.objects.filter(current=True).first()
+
         context.update({
-            'page': link_maker(first_page.id)
+            'page': link_maker(first_page.id),
+            'min_links': DATA_MIN_LINKS_PER_PAGE,
+            'max_links': current_site.links_per_page,
+            'total_pages': current_site.total_pages
         })
         return context
 
@@ -30,7 +37,7 @@ class Page(TemplateView):
         current_site = Site.objects.filter(current=True).first()
         context = super(Page, self).get_context_data(**kwargs)
         if not current_site:
-            messages.add_message(self.request, messages.ERROR, "Set the current site first")
+            messages.add_message(self.request, messages.ERROR, "Set the current site")
             return context
         ps = PageStructure.objects.get(site=current_site, page=page_index)
         links = json.loads(ps.links)
